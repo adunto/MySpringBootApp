da25ff0a606e2b05a887f3d68240ea50edf31249
Kwon Soonmin <sogno6037@gmail.com>
2025-05-09 13:28:56 +0900

    Spring Boot 5일차

--- src/main/java/com/basic/myspringboot/entity/User.java
+++ src/main/java/com/basic/myspringboot/entity/User.java
@@ -1,6 +1,9 @@
 package com.basic.myspringboot.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
 
@@ -17,10 +20,13 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank(message = "Name은 필수 입력항목입니다.")
     @Column(nullable = false)
     private String name;
 
+    @NotBlank(message = "Email은 필수 입력항목입니다.")
     @Column(unique = true, nullable = false)
+    @Email
     private String email;
 
     @Column(nullable = false, updatable = false)
