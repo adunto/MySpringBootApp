97aa9f93382cb6fb3668f3e39ba81c9d2723e06c
Kwon Soonmin <sogno6037@gmail.com>
2025-05-14 17:30:41 +0900

    Update StudentController.java

--- src/main/java/com/basic/myspringboot/controller/StudentController.java
+++ src/main/java/com/basic/myspringboot/controller/StudentController.java
@@ -14,7 +14,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/students")
 @RequiredArgsConstructor
-@CrossOrigin(origins = "http://localhost:8085")
+@CrossOrigin
 public class StudentController {
 
     private final StudentService studentService;
