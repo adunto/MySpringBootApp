52a8d381014f8f64ff9caacdcffae6133b995e62
Kwon Soonmin <sogno6037@gmail.com>
2025-05-14 16:06:13 +0900

    CORS 설정

--- src/main/java/com/basic/myspringboot/controller/StudentController.java
+++ src/main/java/com/basic/myspringboot/controller/StudentController.java
@@ -14,6 +14,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/students")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "http://localhost:8085")
 public class StudentController {
 
     private final StudentService studentService;
